d22814435066fe25b10ea503956d68c619dee7cf
Nate Denys <nate_denys@yahoo.com>
2017-09-30 12:12:23 -0700

    upload template from class and layout plan for application

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* 
+
+what do we need for our zodiac object to work?
+1. A way to take user info (probably input tag)
+2. a way to run the app (button)
+3. we need a function that executes the application 
+	a. save what sign they chose
+	b. return corresponding horoscope info
+		- Need an object for eery horoscope sign
+		- each obhect needs: IMG, horoscope reading, horoscope sign
+	c. put all 12 objects into an array - to loop through easily
+	d. if statement that compares user inut to the name of the sign
+		- inside of the for loop
+	e. display information on the screen
+		- HTML elements with IDs
+		- use innerHTML to change the insides of those elements to data from the objects
+		
+
+*/
